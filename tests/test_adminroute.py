7cd590145537106df21ad1df9aa4211556718ab4
666tut666 <666tut666@users.noreply.github.com>
2022-11-21 10:55:09 +0545

    hmm

--- tests/test_adminroute.py
+++ tests/test_adminroute.py
@@ -3,14 +3,14 @@ from fastapi import status
 
 
 def test_create_user(client):
-    data = {"email": "e@mail.com", "password": "test12"}
+    data = {"email": "e2@mail.com", "password": "test12"}
         # providing dummy data
     response = client.post("/admin", json.dumps(data))
         # gives above data in json format
         ##now we check the o/p using assert
     assert response.status_code == status.HTTP_200_OK
         # 200 ok aaucha thei ho
-    assert response.json()["email"] == "e@mail.com"
+    assert response.json()["email"] == "e2@mail.com"
         # email match check
     assert response.json()["is_active"] == True
         # is_active bool ho, thei check
